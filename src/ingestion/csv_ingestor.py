ef068641f58c61958ace06484096f86a3d272c2b
Niyanta <niyantapandey@gmail.com>
2025-02-02 13:32:26 -0800

    add sample data

--- src/ingestion/csv_ingestor.py
+++ src/ingestion/csv_ingestor.py
@@ -1,6 +1,6 @@
-import pandas as pd 
+import pandas as pd
 import os
-from typing import Optional 
+from typing import Optional
 from sqlalchemy.exc import SQLAlchemyError
 from ..connectors.postgresql import PostgreSQLConnector
 from ..utils.logger import logger
@@ -10,20 +10,20 @@ class CSVIngestor:
         self.connector = PostgreSQLConnector()
         self.engine = self.connector.get_engine()
 
-    def ingest(self, file_path: str, table_name: str, dtype: Optional[dict]= None):
+    def ingest(self, file_path: str, table_name: str, dtype: Optional[dict] = None, parse_dates: Optional[list] = None):
         try:
             if not os.path.exists(file_path):
                 raise FileNotFoundError(f"{file_path} does not exist")
 
-            df = pd.read_csv(file_path, dtype=dtype, parse_dates=True)
+            df = pd.read_csv(file_path, dtype=dtype, parse_dates=parse_dates)
             df.to_sql(
-                table_name, 
+                table_name,
                 self.engine,
                 if_exists="append",
                 index=False
             )
-            logger.info(f"Ingested {len(df)} rows from file_path")
+            logger.info(f"Ingested {len(df)} rows from {file_path}")
             return True
-        except (FileNotFoundError, pd.errors.ParserError, SQlAlchemyError) as e:
+        except (FileNotFoundError, pd.errors.ParserError, SQLAlchemyError) as e:
             logger.error(f"CSV ingestion failed: {str(e)}")
             return False
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,18 +1,26 @@
 import logging
-import sys
 
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-def setup_logger(name: str = "DataIngestion"):
-    logger = logging.getLogger(name)
+    # Create a file handler
+    fh = logging.FileHandler('app.log')
+    fh.setLevel(logging.DEBUG)
+
+    # Create a console handler (optional)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+
+    # Create a formatter and set it for both handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    #Console Handler
-
-    ch = logging.StreamHandler(sys.stdout)
+    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
+
+    # Add the handlers to the logger
     logger.addHandler(fh)
+    logger.addHandler(ch)
 
     return logger
 
 logger = setup_logger()
-
